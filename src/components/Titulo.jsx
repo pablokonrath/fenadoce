62baa2b7c1cfd0685351b8fec61a8e3d7bc13e50
Ricardo Botelho da Silva <rsbotelho.41@gmail.com>
2024-06-27 22:27:37 -0300

    Foi refatorado e atualizado o componente Titulo.jsx

--- src/components/Titulo.jsx
+++ src/components/Titulo.jsx
@@ -1,12 +1,27 @@
+import React from 'react';
+import PropTypes from 'prop-types';
 import './Titulo.css';
 
-function Titulo(conteudo) {
+// Foi criado a desestruturação que permite o acesso diretamente "titulo" e "subtitulo".
+function Titulo({ titulo, subTitulo }) {
   return (
     <div className="titulo">
-      <h1 className="marcador">{conteudo.titulo}</h1>
-      <span>{conteudo.subTitulo}</span>
+      <h1 className="marcador">{titulo}</h1>
+      <span>{subTitulo}</span>
     </div>
   );
 }
+
+// Foi adicionado uma nova camada de validações das props passadas para o componente, podendo evitar futuros bugs.
+Titulo.propTypes = {
+  titulo: PropTypes.string.isRequired,
+  subTitulo: PropTypes.string,
+};
+
+// Foi definido valores padrões para props que são opcionais. Caso o "subTitulo" não seja informado, ele será uma string vazia (`''`). 
+// Desta forma. podendo evitar erros se uma prop opcional não for passada. 
+Titulo.defaultProps = {
+  subTitulo: '',
+};
 
 export default Titulo;
